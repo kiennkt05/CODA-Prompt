16534f13b4c006185153e223c2db27966ba3e3ba
kiennkt05 <kiennkt05@gmail.com>
2026-04-20 20:56:49 +0700

    add imports

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -1,5 +1,19 @@
 from __future__ import absolute_import
 
-from .dataloader import iCIFAR100, iCIFAR10, iIMAGENET_R, iDOMAIN_NET
+from .dataloader import (
+    iCIFAR100,
+    iCIFAR10,
+    iIMAGENET_R,
+    iDOMAIN_NET,
+    iUCF101,
+    iActivityNet,
+)
 
-__all__ = ('iCIFAR100','iCIFAR10','iIMAGENET_R','iDOMAIN_NET')
+__all__ = (
+    'iCIFAR100',
+    'iCIFAR10',
+    'iIMAGENET_R',
+    'iDOMAIN_NET',
+    'iUCF101',
+    'iActivityNet',
+)
